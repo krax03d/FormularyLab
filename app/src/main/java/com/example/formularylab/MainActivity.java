0cb189b7cc78c318f721d4cb37e62098d6bf22c1
Alejandro Pinzon <krax03d@gmail.com>
2019-06-14 22:09:44 -0500

    Listview running on the main screen

--- app/src/main/java/com/example/formularylab/MainActivity.java
+++ app/src/main/java/com/example/formularylab/MainActivity.java
@@ -6,15 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private Spinner spinner1;
     private TextView tv1, tv2;
     private boolean PrimeraVez=true;
+    private ListView ListTrigo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,29 +27,48 @@ public class MainActivity extends AppCompatActivity {
         tv1 = (TextView)findViewById(R.id.tvPrincipal);
         tv2 = (TextView)findViewById(R.id.tv2Principal);
         spinner1=(Spinner)findViewById(R.id.spinnerPrincipal);
-
+        ListTrigo=(ListView)findViewById(R.id.lv_main);
 
         ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.spinnerOpciones,R.layout.spinner_item_size);
         spinner1.setAdapter(adapter);
+        //ArrayList<String>options_main=new ArrayList<String>();
+        //options_main.add("Seleccione");
+        //options_main.add("Calculo");
+        //options_main.add("Fisica");
+        //options_main.add("Trigonometria");
+        //ArrayAdapter<CharSequence> adapterSpinner=new ArrayAdapter(this,android.R.layout.simple_spinner_item,options_main);
+        //spinner1.setAdapter(adapterSpinner);
         spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-                Intent seleccion = null;
                 switch(position){
                     case 1:
-                       Toast.makeText(getApplicationContext(),"Escogiste Calculo",Toast.LENGTH_LONG).show();
+
+                        ArrayAdapter<CharSequence> adapterListView=ArrayAdapter.createFromResource(getApplicationContext(), R.array.Trigo_options,R.layout.listview_item_size);
+                        ListTrigo.setAdapter(adapterListView);
+
                         break;
 
                 }
-
-
-
             }
+
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
+            }
+        });
+        ListTrigo.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent category= null;
+                switch(position){
+                    case 1:
+                            category = new Intent(MainActivity.this,Categories.class);
+
+                        break;
+
+                }
             }
         });
     }
