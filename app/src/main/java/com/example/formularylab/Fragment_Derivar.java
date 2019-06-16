19d988501ab552866bd24c175beb49a262e1c999
Alejandro Pinzon <krax03d@gmail.com>
2019-06-15 20:34:41 -0500

    Button of more information implemented and in operation

--- app/src/main/java/com/example/formularylab/Fragment_Derivar.java
+++ app/src/main/java/com/example/formularylab/Fragment_Derivar.java
@@ -1,27 +1,26 @@
 package com.example.formularylab;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.Toast;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Fragment_Derivar.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Fragment_Derivar#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Fragment_Derivar extends Fragment {
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    Button navigator;
+    View vista;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -33,14 +32,6 @@ public class Fragment_Derivar extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_Derivar.
-     */
     // TODO: Rename and change types and number of parameters
     public static Fragment_Derivar newInstance(String param1, String param2) {
         Fragment_Derivar fragment = new Fragment_Derivar();
@@ -51,20 +42,42 @@ public class Fragment_Derivar extends Fragment {
         return fragment;
     }
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
+
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment__derivar, container, false);
+
+        vista=inflater.inflate(R.layout.fragment_fragment__derivar, container, false);
+        navigator=(Button) vista.findViewById(R.id.bt_derivation_more);
+
+        navigator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getContext(),"El evento",Toast.LENGTH_LONG).show();
+                String page_web="https://es.wikipedia.org/wiki/Derivada";
+                Intent web_navigator=null;
+                Bundle miBundle_web=new Bundle();
+                web_navigator=new Intent(getActivity(),navigator_web.class);
+                miBundle_web.putString("navigator",page_web);
+                web_navigator.putExtras(miBundle_web);
+                startActivity(web_navigator);
+            }
+        });
+        return(vista);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
